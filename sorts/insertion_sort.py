06e718fcf8cabc0d202c97a9fd3a76d9ed358b1d
MalikChettihIA <160392194+MalikChettihIA@users.noreply.github.com>
2024-05-04 16:15:57 +0200

    Insertion sort implémentation

---
+++ sorts/insertion_sort.py
@@ -0,0 +1,11 @@
+# Inerstion Sort Python Implementation.
+# Worst case is in Θ(n*2)
+# Best case is in Θ(n)
+def insert_sort(A, n):
+    for i in range(1, len(A)):
+        key = A[i]
+        j = i -1
+        while j >= 0 and A[j] > key:
+            A[j+1] = A[j]
+            j = j-1
+        A[j+1] = key
